c4b5e8e002fef48065f561edce96c0128f77ec68
Nathalie Kroeker <nathaliekroeker@gmail.com>
2020-04-28 00:52:23 -0400

    Fix a few things that don't solve problems

--- scenes/sprites/spacecraft1.gd
+++ scenes/sprites/spacecraft1.gd
@@ -15,13 +15,13 @@ func get_input():
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += speed
 	if Input.is_action_pressed("ui_up"):
-		velocity.z += speed * 2
+		velocity.z += speed
 	if Input.is_action_pressed("ui_down"):
-		velocity.z -= speed * 2
+		velocity.z -= speed
 		
 func _physics_process(delta):
 	velocity.y = 0
 	velocity.z = 0
 	if is_movable:
 		get_input()
-	velocity = move_and_slide(velocity, Vector3.UP)
+	velocity = move_and_slide(velocity, Vector3.ZERO)
